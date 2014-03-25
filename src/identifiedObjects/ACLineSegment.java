6de424984d406af55931c7ea9a86c33bc403fcf8
Aaron <abs@bakedpota.to>
2014-03-25 15:45:00 -0400

    added new files

--- src/identifiedObjects/ACLineSegment.java
+++ src/identifiedObjects/ACLineSegment.java
@@ -4,6 +4,24 @@ import java.util.Hashtable;
 
 public class ACLineSegment extends ConductingEquipment {
 
+	private ConductingEquipment parentEquipment; 
+	private float length;
+	
+	public ConductingEquipment getParentEquipment() {
+		return parentEquipment;
+	}
+
+	private void setParentEquipment(ConductingEquipment parentEquipment) {
+		this.parentEquipment = parentEquipment;
+	}
+	
+	private void ConnectivityNode getNeighbor(Terminal term) {
+		this.parentEquipment = newParentEquipment;
+		
+	
+	}
+	
+	
 	public ACLineSegment(Hashtable<String, String> objValues) {
 		super(objValues);
 		// TODO Auto-generated constructor stub
